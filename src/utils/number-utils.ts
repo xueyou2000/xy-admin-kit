18e19689e1679748abd8b6d86924f61f254051ba
XueYou <xueyoucd@gmail.com>
2021-09-09 16:36:43 +0800

    fix: 修复精确数值运算方法

--- src/utils/number-utils.ts
+++ src/utils/number-utils.ts
@@ -51,7 +51,19 @@ export function clamp(i: number, min: number, max: number) {
  * @returns 两个参数的和  num1 + num2
  */
 export function add(num1: number, num2: number) {
-  return (num1 * FIX_NUMBER + num2 * FIX_NUMBER) / FIX_NUMBER;
+  var r1, r2, m;
+  try {
+    r1 = num1.toString().split(".")[1].length;
+  } catch (e) {
+    r1 = 0;
+  }
+  try {
+    r2 = num2.toString().split(".")[1].length;
+  } catch (e) {
+    r2 = 0;
+  }
+  m = Math.pow(10, Math.max(r1, r2));
+  return (num1 * m + num2 * m) / m;
 }
 
 /**
@@ -61,7 +73,64 @@ export function add(num1: number, num2: number) {
  * @returns 两个参数的差  num1 - num2
  */
 export function sub(num1: number, num2: number) {
-  return (num1 * FIX_NUMBER - num2 * FIX_NUMBER) / FIX_NUMBER;
+  var r1, r2, m, n;
+  try {
+    r1 = num1.toString().split(".")[1].length;
+  } catch (e) {
+    r1 = 0;
+  }
+  try {
+    r2 = num2.toString().split(".")[1].length;
+  } catch (e) {
+    r2 = 0;
+  }
+  m = Math.pow(10, Math.max(r1, r2));
+  n = r1 >= r2 ? r1 : r2;
+  return ((num1 * m - num2 * m) / m).toFixed(n);
+}
+
+/**
+ * 提供精确的乘法运算
+ * @param num1
+ * @param num2
+ * @returns
+ */
+export function multip(num1: number, num2: number) {
+  var m = 0,
+    s1 = num1.toString(),
+    s2 = num2.toString();
+  try {
+    m += s1.split(".")[1].length;
+  } catch (e) {}
+  try {
+    m += s2.split(".")[1].length;
+  } catch (e) {}
+  return (
+    (Number(s1.replace(".", "")) * Number(s2.replace(".", ""))) /
+    Math.pow(10, m)
+  );
+}
+
+/**
+ * 提供精确的除法运算
+ * @param num1
+ * @param num2
+ * @returns
+ */
+export function division(num1: number, num2: number) {
+  var t1 = 0,
+    t2 = 0,
+    r1,
+    r2;
+  try {
+    t1 = num1.toString().split(".")[1].length;
+  } catch (e) {}
+  try {
+    t2 = num2.toString().split(".")[1].length;
+  } catch (e) {}
+  r1 = Number(num1.toString().replace(".", ""));
+  r2 = Number(num2.toString().replace(".", ""));
+  return multip(r1 / r2, Math.pow(10, t2 - t1));
 }
 
 /**
